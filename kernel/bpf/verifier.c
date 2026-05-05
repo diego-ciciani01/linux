397a9a65a6c48968ba931655fc4a9805c13c0ff7
diego-ciciani01 <diego.ciciani@gmail.com>
2026-05-05 14:11:11 +0200

    typo fix error in verifier switch

--- kernel/bpf/verifier.c
+++ kernel/bpf/verifier.c
@@ -15650,6 +15650,7 @@ static int is_scalar_branch_taken(struct bpf_verifier_env *env, struct bpf_reg_s
 	                return 1;
 	        else if (umin1 >= umax2)
 	                return 0;
+		break;
 	case BPF_JSLT:
 		if (smax1 < smin2)
 			return 1;
@@ -16288,7 +16289,7 @@ static void sync_linked_regs(struct bpf_verifier_env *env, struct bpf_verifier_s
 	}
 }
 
-static int check_cond_jmp_opBPF_JCOND(struct bpf_verifier_env *env,
+static int check_cond_jmp_op(struct bpf_verifier_env *env,
 			     struct bpf_insn *insn, int *insn_idx)
 {
 	struct bpf_verifier_state *this_branch = env->cur_state;
